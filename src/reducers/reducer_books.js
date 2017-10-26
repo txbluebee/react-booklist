8eb333cf459681b29a7576d98227d1248c5c2c30
Brian Fan <kuomu.fan@yahoo.com>
2017-10-25 21:13:39 -0700

    practice redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title: 'The Mist', pages: 120},
+        { title: 'It', pages: 150},
+        { title: 'The Datk Tower', pages: 300},
+        { title: 'DreamCatcher', pages: 50}
+    ];
+}
